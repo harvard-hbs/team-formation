967f107369c3137a6f18d651326674581d09d325
bbenson-hbs <125278138+bbenson-hbs@users.noreply.github.com>
2023-10-17 08:26:32 -0400

    A start at packaging

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open("team_formation/_version.txt") as f:
+    version = f.read().strip()
+
+setup(
+    name="team_formation",
+    version=version,
+    description=(
+        "A tool to form teams from a larger group based "
+        "on clustering and diversity constraints"
+    ),
+    url="https://github.com/harvard-hbs/team-formation/",
+    author="Brent Benson",
+    license="MIT",
+    install_requires=[
+        "pandas",
+        "ortools",
+    ],
+)
--- team_formation/__init__.py
+++ team_formation/__init__.py
@@ -1,3 +1,11 @@
+import os
+
+package_path = os.path.dirname(os.path.realpath(__file__))
+version_file_path = os.path.join(package_path, "_version.txt")
+
+with open(version_file_path) as f:
+    __version__ = f.read().strip()
+
 from .team_assignment import TeamAssignment, SolutionCallback
-from .team_assignment import small_test, big_test
+from .team_assignment import small_test
 
