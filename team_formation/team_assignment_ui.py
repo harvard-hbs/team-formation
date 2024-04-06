e44c8da4395a68297fb3eb4c8a0c7e640dc0168f
Brent Benson <bbenson@hbs.edu>
2024-04-05 20:02:15 -0400

    Fix multi-line f-string

--- team_formation/team_assignment_ui.py
+++ team_formation/team_assignment_ui.py
@@ -1,17 +1,45 @@
 import streamlit as st
 import pandas as pd
-import threading
+import datetime
+import humanize
 
-from team_assignment import TeamAssignment, SolutionCallback
+from team_assignment import TeamAssignment
+from ortools.sat.python import cp_model
+
+class SolutionCallback(cp_model.CpSolverSolutionCallback):
+    def __init__(self):
+        cp_model.CpSolverSolutionCallback.__init__(self)
+        self.start_time = datetime.datetime.now()
+
+    def on_solution_callback(self):
+        objective_value = self.ObjectiveValue()
+        num_conflicts = self.NumConflicts()
+        cur_time = datetime.datetime.now()
+        time_diff = cur_time - self.start_time
+        time_diff_human = humanize.naturaldelta(time_diff)
+        print(
+            f"Elapsed time: {time_diff}, Number of conflicts: " +
+            f"{num_conflicts}, Objective value: {objective_value}"
+        )
+        if time_diff.seconds > 10:
+            self.StopSearch()
 
 def roster_upload_callback():
     roster_upload = st.session_state["roster_upload"]
     if roster_upload.type == "text/csv":
-         roster = pd.read_csv(roster_upload)
+         roster = (pd.read_csv(roster_upload)
+                   .pipe(create_list_columns))
     elif roster_upload.type == "application/json":
         roster = pd.read_json(roster_upload)
     st.session_state["roster"] = roster
 
+def create_list_columns(df):
+    new_df = df.copy()
+    for list_col in (col for col in df.columns
+                     if col.endswith("_list")):
+        new_df[list_col] = new_df[list_col].map(lambda str_val: str_val.split(";"))
+    return new_df
+    
 def constraints_upload_callback():
     constraints_upload = st.session_state["constraints_upload"]
     if constraints_upload.type == "text/csv":
@@ -29,8 +57,9 @@ def generate_teams_callback():
         st.session_state["constraints"],
         st.session_state["target_team_size"])
     st.session_state["team_assignment"] = team_assignment
+    callback = SolutionCallback()
     with st.spinner("Generating teams..."):
-        team_assignment.solve()
+        team_assignment.solve(solution_callback=callback)
         if team_assignment.solution_found:
             st.session_state["solution_found"] = True
             roster_teams = (team_assignment
